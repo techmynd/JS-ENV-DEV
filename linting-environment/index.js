44ab4737cdad479944ac4d1c037bcc87d03f3ea3
javed khalil <admin@techmynd.com>
2019-07-20 03:32:54 +0500

    update

---
+++ linting-environment/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const x = 1;
+
+/*
+  // enable eslint and prettier in vscode and use packages in this project as dev dependency
+  Reference
+  ---------
+  ESLint Rules - https://eslint.org/docs/rules/
+  Prettier Options - https://prettier.io/docs/en/options.html
+  Airbnb Style Guide - https://github.com/airbnb/javascript
+*/
